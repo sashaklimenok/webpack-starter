9c54cc1008bf10935de85703f3464ee7bf25bb6d
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2020-05-18 12:55:31 +0300

    change structure project

---
+++ src/eloquent-javaScript/chapter-4 (Objects and Arrays)/flattieng.ts
@@ -0,0 +1,15 @@
+import { UNICODES } from "../mock";
+
+const unicodeRanges = UNICODES.filter((el: any, i) => el.living && i < 2).map((unicode) => unicode.ranges);
+
+function flattening(arr: any[]): any {
+  const result: any = [];
+  return function req(arr) {
+    for (const item of arr) {
+      item instanceof Array ? req(item) : result.push(item);
+    }
+    return result
+  }(arr);
+}
+
+flattening(unicodeRanges);
--- src/eloquent-javaScript/index.ts
+++
@@ -1,4 +0,0 @@
-import { findSolution } from './chapter-3/functions';
-
-console.log(findSolution(24));
-console.log('dsd');
