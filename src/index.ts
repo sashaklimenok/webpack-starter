b6a00a6a40a404149025cc851a52abf1943e4d12
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2020-05-22 09:14:04 +0300

    add chache decarator

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { printNumbers } from "./eloquent-javaScript/chapter-5 (High order functions)/timers";
+import './eloquent-javaScript/chapter-5 (High order functions)/decorators';
 
 printNumbers(0, 5);
