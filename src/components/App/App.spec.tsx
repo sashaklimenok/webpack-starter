96603c37662918b81b9f3595a968b3f22ec63120
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2020-08-01 10:30:07 +0300

    chore: fix test for APP

--- src/components/App/App.spec.tsx
+++ src/components/App/App.spec.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import { App } from './App';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-
 
 describe('Test', () => {
   it('should return 10', () => {
